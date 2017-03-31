57b360211a049997ea1d1b9141b07e1a33553637
miftahul jannah <miftah074@gmail.com>
2017-03-31 07:49:14 +0700

    ayo lanjutkan

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl601131835/kalkulator/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl601131835/kalkulator/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -169,4 +171,5 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
+
 }
